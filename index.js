a4316d070dcacbd2569b5c03b1606cb09a9422ef
aurelia <aurelia.zigman@gmail.com>
2019-10-02 11:06:43 +0200

    premier commit avec node module

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.magenta.bold('Elise'));
+console.log(chalk.blue('Baptiste'));
+console.log(chalk.red.bold('Martin'));
+console.log(chalk.green('Titouan'));
